1e10ed9365ba89bd43d714ee9033c813d1e97098
Raj Tushal <tushalkhatri902@gmail.com>
2024-09-09 22:52:43 +0500

    Resolved merge conflict

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/React-Restaurant/', // Keep this line if you need it
+})
